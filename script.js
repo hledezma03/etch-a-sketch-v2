0725c8c13d145af5df9eb3d500196ee89e1bff8f
Hector <hledezma910@gmail.com>
2024-11-26 18:21:16 -0400

    Create the button event and randomColor function

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
 const gridContainer = document.querySelector('.grid-container');
+const gridSize = document.querySelector('#grid-size');
+const gridBtn = document.querySelector('#create-grid');
 
-for (let f = 0; f < 16; f++) {
-    for (let c = 0; c < 16; c++) {
-        const square = document.createElement('div');
-        square.classList.add('grid-item');
-        square.style.width = `${100 / 16}%`;
-        square.style.height = `${100 / 16}%`;
-        square.style.border = '1px solid black';
-        gridContainer.appendChild(square);
-    };
+const randomColor = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    
+    return `rgb(${r},${g},${b})`;
 };
 
-
-const gridItem = document.querySelectorAll('.grid-item');
-
-gridItem.forEach(item => {
+const createGrid = (size = 16) => {
+    for (let f = 0; f < size; f++) {
+        for (let c = 0; c < size; c++) {
+            const square = document.createElement('div');
+            square.classList.add('grid-item');
+            square.style.width = `${100 / size}%`;
+            square.style.height = `${100 / size}%`;
+            square.style.border = '1px solid black';
+            gridContainer.appendChild(square);
+        };
+    };
+    const gridItem = document.querySelectorAll('.grid-item');
+    gridItem.forEach(item => {
     item.addEventListener("mouseover", () => {
-        item.style.backgroundColor = 'red';
-    })
+        item.style.backgroundColor = randomColor();
+    });
+});
+};
+
+gridBtn.addEventListener("click", ()=> {
+    if (!gridSize.value) {
+        return;
+    } else {
+        size = gridSize.value;
+        gridContainer.innerHTML = ''
+        createGrid(size);
+    }
 })
+
+
+createGrid();
+
+
+
 
